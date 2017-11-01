f64c41ed006f8de07ee2c141430857dd940eb895
Camilla Bontogon <camillabontogon@Camillas-MacBook-Pro.local>
2017-11-01 16:47:48 -0500

    Dev and prod dbs

--- app/controllers/user_careers_controller.rb
+++ app/controllers/user_careers_controller.rb
@@ -55,10 +55,21 @@ class UserCareersController < ApplicationController
     session[:selected_career] = @user_career.id
     @user_jobs = UserJob.where(user_career_id: @user_career.id)
 
-    @connection = ActiveRecord::Base.establish_connection(
-        adapter: "sqlite3",
-        database: "db/development.sqlite3"
-    )
+    if Rails.env.development?
+      @connection = ActiveRecord::Base.establish_connection(
+          adapter: "sqlite3",
+          database: "db/development.sqlite3"
+      )
+    elsif Rails.env.production?
+      @connection = ActiveRecord::Base.establish_connection(
+          :adapter  => "postgresql",
+          :host     => "ec2-174-129-253-133.compute-1.amazonaws.com",
+          :username => "bixsnitgldzbdm",
+          :password => "8da72462b4a7562d73f7846d0d039dac86d44ae846520d87c2cbaa72bc14789e",
+          :port     => "5432",
+          :database => "d3isejcb2j35k"
+      )
+    end
 
     skill_hours = "select career_skills.skill_id, career_skills.weight, skill_hours.totalhours from career_skills, (select activity_skills.skill_id as skillid, sum(activities.hours) as totalhours from activities, activity_skills where activities.id = activity_skills.activity_id and activities.user_id = '" + session[:user_id].to_s + "' group by activity_skills.skill_id) as skill_hours where career_skills.skill_id = skill_hours.skillid and career_skills.user_career_id = '" + session[:selected_career].to_s + "';"
     @results = @connection.connection.execute(skill_hours)
--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,10 +1,21 @@
 class UserSkillsController < ApplicationController
 
   def index
-    @connection = ActiveRecord::Base.establish_connection(
-        adapter: "sqlite3",
-        database: "db/development.sqlite3"
-    )
+    if Rails.env.development?
+      @connection = ActiveRecord::Base.establish_connection(
+          adapter: "sqlite3",
+          database: "db/development.sqlite3"
+      )
+    elsif Rails.env.production?
+      @connection = ActiveRecord::Base.establish_connection(
+          :adapter  => "postgresql",
+          :host     => "ec2-174-129-253-133.compute-1.amazonaws.com",
+          :username => "bixsnitgldzbdm",
+          :password => "8da72462b4a7562d73f7846d0d039dac86d44ae846520d87c2cbaa72bc14789e",
+          :port     => "5432",
+          :database => "d3isejcb2j35k"
+      )
+    end
     user_skills = "select activity_skills.skill_id as skillid, sum(activities.hours) as totalhours from activities, activity_skills where activities.id = activity_skills.activity_id and activities.user_id = '" + session[:user_id].to_s + "' group by activity_skills.skill_id, activities.user_id"
     @results = @connection.connection.execute(user_skills)
 
