a0895c2040db5060510e3b1f69bbc5f0a985b70b
Camilla Bontogon <camillabontogon@Camillas-MacBook-Pro.local>
2017-12-10 19:34:23 -0600

    update database config

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -9,11 +9,11 @@ class UserSkillsController < ApplicationController
     elsif Rails.env.production?
       @connection = ActiveRecord::Base.establish_connection(
           :adapter  => "postgresql",
-          :host     => "ec2-174-129-253-133.compute-1.amazonaws.com",
-          :username => "bixsnitgldzbdm",
-          :password => "8da72462b4a7562d73f7846d0d039dac86d44ae846520d87c2cbaa72bc14789e",
+          :host     => "ec2-23-21-220-32.compute-1.amazonaws.com",
+          :username => "nuuytljfdymvyu",
+          :password => "4adbbec8515ec7f337673a5e76f1c92b470ba431b472a7b0c9a3ac3f887976ba",
           :port     => "5432",
-          :database => "d3isejcb2j35k"
+          :database => "dfnme5l6or8t88"
       )
     end
     user_skills = "select competency_skills.skill_id as skillid, sum(activityhours) as totalhours from competency_skills, (select activity_competencies.competency_id as competencyid, sum(activities.hours) as activityhours from activities, activity_competencies where activities.id = activity_competencies.activity_id and activities.user_id = '" + session[:user_id].to_s + "' group by activity_competencies.competency_id) as competency_hours where competency_hours.competencyid = competency_skills.competency_id group by skillid"
